d874284c5ff1f8d20d752fc59c46258c9f1f0cd9
Hanfl <m864656893@126.com>
2015-11-17 13:26:03 +0800

    阶段提交

--- examinationSystem/src/main/java/com/qdjxd/examination/examacitvity/fragment/QuestionFragment.java
+++ examinationSystem/src/main/java/com/qdjxd/examination/examacitvity/fragment/QuestionFragment.java
@@ -2,21 +2,25 @@ package com.qdjxd.examination.examacitvity.fragment;
 
 import android.app.Activity;
 import android.app.ListFragment;
-import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
 
 import com.qdjxd.examination.R;
+import com.qdjxd.examination.examacitvity.AutonomousExamActivity;
 import com.qdjxd.examination.examacitvity.adapter.AnswerListAdapter;
 import com.qdjxd.examination.examacitvity.bean.AnswerInfo;
 import com.qdjxd.examination.examacitvity.bean.QuestionInfo;
 import com.qdjxd.examination.utils.DebugLog;
 
 import java.util.Arrays;
+import java.util.Iterator;
 import java.util.List;
 
 /**
@@ -26,7 +30,9 @@ public class QuestionFragment extends ListFragment {
     public static final String TAG = QuestionFragment.class.getSimpleName();
     private View questionView;
 
-    ArrayAdapter<AnswerInfo> adapter;
+   // private  ListView listView;
+
+    private AnswerListAdapter adapter;
     private QuestionInfo questionInfo;
     private List<AnswerInfo> answerItem;
     private final List<String> list = Arrays.asList("正确","错误");
@@ -41,7 +47,7 @@ public class QuestionFragment extends ListFragment {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        DebugLog.i(TAG);
+        //DebugLog.i(TAG);
         Activity activity = getActivity();
         select = new boolean[]{false,false,false,false,false};
         if(answerItem.size()==0){
@@ -59,20 +65,23 @@ public class QuestionFragment extends ListFragment {
                 answerItem.add(answerInfo);
             }
         }
-        adapter = new AnswerListAdapter(activity,answerItem);
+        adapter = new AnswerListAdapter(activity,questionInfo);
     }
 
     @Override
     public void onActivityCreated(Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
         setListAdapter(null);
+        /*listView =  this.getListView();
+        listView.setDividerHeight(0);
+        listView.setCacheColorHint(0);*/
+
         LayoutInflater inflater = getActivity().getLayoutInflater();
         //问题显示页面
         questionView = inflater.inflate(R.layout.activity_exam_autonomous_item,null);
         //题目内容
         TextView questionTx = (TextView) questionView.findViewById(R.id.choose_question_content);
-        questionTx.setText(num+"."+questionInfo.qcontent);
-        DebugLog.i(questionInfo.qcontent);
+        questionTx.setText(num + "." + questionInfo.qcontent);
         //对题目类型进行判断,更改题目类型图标
         if(questionInfo.typeid.equals("1")){
             questionTx.setCompoundDrawablesWithIntrinsicBounds(
@@ -86,36 +95,71 @@ public class QuestionFragment extends ListFragment {
         }
         setListAdapter(adapter);
         getListView().addHeaderView(questionView);
+        getListView().setCacheColorHint(0);
+        getListView().setDividerHeight(0);
+        getListView().setSelector(R.color.white);
+
+        getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //DebugLog.i("onListItemClick" + position);
+                TextView tx = (TextView) view.findViewById(R.id.item_answer);
+                int length = select.length;
+                for (int i = 0; i < length; i++) {
+                    //对当前点击项目做出判断
+                    if ((i + 1) == position) {
+                        if (select[i]) {
+                            //DebugLog.i("点击当前选中项，取消选择");
+                            setTextView(tx, position, select[i]);
+                            select[i] = false;
+                        } else {
+                            //DebugLog.i("初次点击选中");
+                            setTextView(tx, position, select[i]);
+                            select[i] = true;
+                        }
+                    }
+                    //如果之前被选中（select[i]是true）,但不是当前点击项
+                    if ((i + 1) != position && select[i]) {
+                        //DebugLog.i("点击其他项目，取消之前选择");
+                        setTextView(tx, position, select[i]);
+                        select[i] = false;
+                    }
+                   // DebugLog.i(select[i]);
+
+                }
+                //记录选项
+                for (int i = 0; i < length; ++i) {
+                    //DebugLog.i("记录选项");
+                    String ans = ((char) (65 + i)) + "";
+                    if (select[i] && !questionInfo.selectAnswer.contains(ans)) {
+                        questionInfo.selectAnswer.add(ans);
+                    } else if (!select[i] && questionInfo.selectAnswer.contains(ans)) {
+                        questionInfo.selectAnswer.remove(ans);
+                    }
+                }
+                new Handler().postDelayed(new Runnable() {
+                    @Override
+                    public void run() {
+                        if(questionInfo.selectAnswer!=null) {
+                            adapter.questionInfo = questionInfo;
+                            adapter.notifyDataSetChanged();
+                        }
+                    }
+                },400);
+            }
+        });
+    }
+    @Override
+    public void onPause() {
+        super.onPause();
     }
 
     @Override
-    public void onListItemClick(ListView l, View v, int position, long id) {
-        super.onListItemClick(l, v, position, id);
-        DebugLog.i("onListItemClick" + position);
-        TextView tx = (TextView) v.findViewById(R.id.item_answer);
-        for(int i=0;i<select.length;i++){
-            //对当前点击项目做出判断
-            if((i+1)==position){
-                if(select[i]){
-                    DebugLog.i("点击当前选中项，取消选择");
-                    setTextView(tx, position, select[i]);
-                    select[i] = false;
-                }else{
-                    DebugLog.i("初次点击选中");
-                    setTextView(tx,position,select[i]);
-                    select[i] = true;
-                }
-            }
-            //如果之前被选中（select[i]是true）,但不是当前点击项
-            if((i+1)!=position&&select[i]){
-                DebugLog.i("点击其他项目，取消之前选择");
-                setTextView(tx,position,select[i]);
-                select[i] = false;
-            }
-            DebugLog.i(select[i]);
-        }
-
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        DebugLog.i("onAttach(*****)");
     }
+
     private void setTextView(TextView tx,int position,boolean flag){
         switch (position){
             case 1:
@@ -163,7 +207,7 @@ public class QuestionFragment extends ListFragment {
         }
     }
     private void setTextViewDrawable(TextView tx,int id){
-        DebugLog.i("setTextViewDrawable");
         tx.setCompoundDrawablesWithIntrinsicBounds(getActivity().getResources().getDrawable(id), null, null, null);
     }
+
 }
