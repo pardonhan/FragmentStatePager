5238467c3228adc2ca3c70bccff18d94cf8bfcfd
pardonhan <m864656893@126.com>
2016-07-06 17:45:26 +0800

    exchange

--- examinationSystem/src/main/java/com/qdjxd/examination/BaseActivity.java
+++ examinationSystem/src/main/java/com/qdjxd/examination/BaseActivity.java
@@ -21,12 +21,11 @@ public class BaseActivity extends Activity {
 	 * 轻量数据储存方式
 	 */
 	public SharedPreferencesHelper sp;
+
 	/**
 	 * 关于屏幕参数的类
 	 */
 	public DensityUtil du;
-
-	public String DB_PATH;
 	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -35,92 +34,11 @@ public class BaseActivity extends Activity {
 		du = new DensityUtil(this);
 	}
 
+	@Override
 	protected void onPause() {
 		super.onPause();
 		if (finishActivityOnPause) {
 			finish();
 		}
-	}
-
-	protected LinearLayout menu;
-	protected LinearLayout main;
-	protected LayoutParams menuParams;
-	protected LayoutParams mainParams;
-	protected boolean mIsShow = true;
-
-	/**
-	 * 判断SD卡上的文件夹是否存在
-	 * */
-	public boolean isFileExist(String str) {
-		File file = new File(str);
-		return file.exists();
-	}
-
-
-	/**
-	 * 侧边栏菜单动画效果
-	 * 
-	 * @author huazi
-	 * 
-	 */
-	protected class ShowMenuAsyncTask extends AsyncTask<Integer, Integer, Integer> {
-
-		@Override
-		protected Integer doInBackground(Integer... params) {
-			int leftMargin = mainParams.leftMargin;
-
-			while (true) {
-				leftMargin += params[0];
-				if (params[0] > 0 && leftMargin >= 0) {
-					leftMargin = 0;
-					break;
-				} else if (params[0] < 0 && leftMargin <= -menuParams.width) {
-					leftMargin = -menuParams.width;
-					break;
-				}
-				publishProgress(leftMargin);
-				try {
-					Thread.sleep(30);
-				} catch (InterruptedException e) {
-					e.printStackTrace();
-				}
-			}
-
-			return leftMargin;
-		}
-
-		@Override
-		protected void onProgressUpdate(Integer... values) {
-			super.onProgressUpdate(values);
-			mainParams.leftMargin = values[0];
-			main.setLayoutParams(mainParams);
-		}
-
-		@Override
-		protected void onPostExecute(Integer result) {
-			super.onPostExecute(result);
-			mainParams.leftMargin = result;
-			main.setLayoutParams(mainParams);
-		}
-
-	}
-
-	protected void showMenu(boolean isShow) {
-		if (isShow) {
-			mIsShow = true;
-			new ShowMenuAsyncTask().execute(50);
-		} else {
-			mIsShow = false;
-			new ShowMenuAsyncTask().execute(-50);
-		}
-	}
-
-	@Override
-	public void onBackPressed() {
-		if (!mIsShow) {
-			showMenu(!mIsShow);
-			return;
-		}
-		super.onBackPressed();
 	}
 }
--- examinationSystem/src/main/java/com/qdjxd/examination/utils/PublicUtils.java
+++ examinationSystem/src/main/java/com/qdjxd/examination/utils/PublicUtils.java
@@ -1,5 +1,6 @@
 package com.qdjxd.examination.utils;
 
+import java.io.File;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -7,59 +8,60 @@ import java.util.Random;
 import java.util.Set;
 
 public class PublicUtils {
-	/**
-	 *  生成指定范围的指定个数的不重复的随机数
-	 *  返回一个List保存已经生成的随机数
-	 * @param size 随机数生成范围
-	 * @param count 随机数生成个数
-	 * @return
-	 */
-	public static List<Integer> getRandomList(int size,int count){
-		List<Integer> ranList = new ArrayList<Integer>();
-		int num = 0;
-		Random rand = new Random();  
-		boolean[] bool = new boolean[size];
-		//
-		for(int i= 0;i<count;i++){
-			//do{}while();循环判断是否产生了重复的随机数，如果重复则重新生成
-			do{
-				num = rand.nextInt(size);	
-			}while(bool[num]);
-			bool[num] = true;
-			ranList.add(num);
-		}
-		return ranList;
-	}
-	/**
-	 * 比较set中的数据是否相同
-	 * @param set1
-	 * @param set2
-	 * @return
-	 */
-	@SuppressWarnings("rawtypes")
-	public static boolean isSetEqual(Set set1, Set set2) {
+    /**
+     * 生成指定范围的指定个数的不重复的随机数
+     * 返回一个List保存已经生成的随机数
+     *
+     * @param size  随机数生成范围
+     * @param count 随机数生成个数
+     * @return list
+     */
+    public static List<Integer> getRandomList(int size, int count) {
+        List<Integer> ranList = new ArrayList<>();
+        int num = 0;
+        Random rand = new Random();
+        boolean[] bool = new boolean[size];
+        for (int i = 0; i < count; i++) {
+            //do{}while();循环判断是否产生了重复的随机数，如果重复则重新生成
+            do {
+                num = rand.nextInt(size);
+            } while (bool[num]);
+            bool[num] = true;
+            ranList.add(num);
+        }
+        return ranList;
+    }
 
-		if (set1 == null && set2 == null) {
-			return true; // Both are null
-		}
+    /**
+     * 比较set中的数据是否相同
+     *
+     * @param set1 set1
+     * @param set2 set2
+     * @return boolean
+     */
+    public static boolean isSetEqual(Set set1, Set set2) {
+        if (set1 == null && set2 == null) {
+            return true; // Both are null
+        }
+        if (set1 == null || set2 == null || set1.size() != set2.size()|| set1.size() == 0 || set2.size() == 0) {
+            return false;
+        }
+        Iterator ite2 = set2.iterator();
+        boolean isFullEqual = true;
+        while (ite2.hasNext()) {
+            if (!set1.contains(ite2.next())) {
+                isFullEqual = false;
+            }
+        }
+        return isFullEqual;
+    }
 
-		if (set1 == null || set2 == null || set1.size() != set2.size()
-				|| set1.size() == 0 || set2.size() == 0) {
-			return false;
-		}
+    /**
+     * 判断SD卡上的文件夹是否存在
+     */
+    public boolean isFileExist(String str) {
+        File file = new File(str);
+        return file.exists();
+    }
 
-		@SuppressWarnings("unused")
-		Iterator ite1 = set1.iterator();
-		Iterator ite2 = set2.iterator();
-
-		boolean isFullEqual = true;
-
-		while (ite2.hasNext()) {
-			if (!set1.contains(ite2.next())) {
-				isFullEqual = false;
-			}
-		}
-
-		return isFullEqual;
-	}
 }
